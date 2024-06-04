fcdc261b61d1addf6b243a88f1194ad36fd9df84
I-Jurij <yjurij@gmail.com>
2024-06-04 14:38:00 +0300

    added auto update_at to tables in db

--- bin/create_sql.php
+++ bin/create_sql.php
@@ -23,9 +23,17 @@ $users = 'CREATE TABLE IF NOT EXISTS "users"
 		"email_verified" INTEGER DEFAULT 0, 
 		"auth_token" INTEGER, 
 		"role" TEXT,
-		"created_at" INTEGER,
-		"updated_at" INTEGER
+		"created_at" TEXT NOT NULL DEFAULT current_timestamp,
+		"updated_at" TEXT NOT NULL DEFAULT current_timestamp
 	)';
+$users_updated_at_trigger = 'CREATE TRIGGER update_users_updated_at
+		AFTER UPDATE ON users
+		WHEN old.updated_at <> current_timestamp
+		BEGIN
+			UPDATE users
+			SET updated_at = CURRENT_TIMESTAMP
+			WHERE id = OLD.id;
+		END';
 $clients = 'CREATE TABLE IF NOT EXISTS "clients" 
 	(	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
 		"name" TEXT, 
@@ -34,9 +42,17 @@ $clients = 'CREATE TABLE IF NOT EXISTS "clients"
 		"email" UNIQUE ON CONFLICT ROLLBACK, 
 		"email_verified" INTEGER DEFAULT 0,
 		"auth_token" INTEGER, 
-		"created_at" INTEGER,
-		"updated_at" INTEGER
+		"created_at" TEXT NOT NULL DEFAULT current_timestamp,
+		"updated_at" TEXT NOT NULL DEFAULT current_timestamp
 	)';
+$cliens_updated_at_trigger = 'CREATE TRIGGER update_clients_updated_at
+		AFTER UPDATE ON clients
+		WHEN old.updated_at <> current_timestamp
+		BEGIN
+			UPDATE clients
+			SET updated_at = CURRENT_TIMESTAMP
+			WHERE id = OLD.id;
+		END';
 $pages = 'CREATE TABLE IF NOT EXISTS pages
 	(	"id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
 		"alias" VARCHAR(100) UNIQUE  ON CONFLICT ROLLBACK NOT NULL,
@@ -51,15 +67,25 @@ $pages = 'CREATE TABLE IF NOT EXISTS pages
 		"published" INTEGER,
 		"access_level" VARCHAR(10), 
 		"admin" INTEGER,
-		"created_at" INTEGER,
-		"updated_at" INTEGER
+		"created_at" TEXT NOT NULL DEFAULT current_timestamp,
+		"updated_at" TEXT NOT NULL DEFAULT current_timestamp
 	)';
-
+$pages_updated_at_trigger = 'CREATE TRIGGER update_pages_updated_at
+		AFTER UPDATE ON pages
+		WHEN old.updated_at <> current_timestamp
+		BEGIN
+			UPDATE pages
+			SET updated_at = CURRENT_TIMESTAMP
+			WHERE id = OLD.id;
+		END';
 $create_sqls = [
     'roles' => $roles,
     'permissions' => $permissions,
     'roles_permissions' => $roles_permissions,
     'users' => $users,
+    'users_updated_at_trigger' => $users_updated_at_trigger,
     'pages' => $pages,
+    'pages_updated_at_trigger' => $pages_updated_at_trigger,
     'clients' => $clients,
+    'cliens_updated_at_trigger' => $cliens_updated_at_trigger,
 ];
