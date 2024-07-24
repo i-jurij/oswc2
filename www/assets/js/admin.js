b29b88739f1bb9ed81bd6d1289a02ebd0168fe90
I-Jurij <yjurij@gmail.com>
2024-07-24 21:34:53 +0300

    update

--- www/assets/js/admin.js
+++ www/assets/js/admin.js
@@ -33,7 +33,6 @@
 /******/ 	})();
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
 (() => {
 
