d853dbda07147e646b0264889bf806169b869cf4
I-Jurij <yjurij@gmail.com>
2024-11-14 23:20:26 +0300

    bug fix

--- app/UI/BasePresenter.php
+++ app/UI/BasePresenter.php
@@ -7,30 +7,22 @@ namespace App\UI;
 abstract class BasePresenter extends \Nette\Application\UI\Presenter
 {
     use Accessory\Breadcrumb;
-    use Accessory\LinkFromFileSystem;
-    use Accessory\GetKeyValueRecursive;
-
-    protected array $dirList;
-
-    public function __construct()
-    {
-        parent::__construct();
-        $this->dirList = $this->linkFromFileSystem(APPDIR.DIRECTORY_SEPARATOR.'UI');
-    }
 
     public function beforeRender()
     {
         $this->template->breadcrumb = $this->getBC();
-        $this->template->dirList = $this->dirList;
-        $this->template->currentPageMenu = $this->getKeyValueRec(end($this->template->breadcrumb)['full'], $this->dirList);
         $this->template->shared_templates = APPDIR.DIRECTORY_SEPARATOR.'UI'.DIRECTORY_SEPARATOR.'shared_templates'.DIRECTORY_SEPARATOR;
     }
 }
 
 class BaseTemplate extends \Nette\Bridges\ApplicationLatte\Template
 {
+    public \Nette\Security\User $user;
+    public string $basePath;
+    public string $baseUrl;
+    public array $flashes;
+    public object $presenter;
+    public object $control;
     public array $breadcrumb;
-    public array $dirList;
-    public array $currentPageMenu;
     public string $shared_templates;
 }
